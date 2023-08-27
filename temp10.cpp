bf9a50348bb3682bab6846db8ca1376b3222ab7e
Mohammad Abdullah-Al-Sajid Chowdhury <75419355+SajidAbdullah729@users.noreply.github.com>
2023-08-27 17:18:48 +0600

    Update temp10.cpp

--- temp10.cpp
+++ temp10.cpp
@@ -1,1 +1,45 @@
 
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+#define FF ios_base::sync_with_stdio(0);cin.tie(0)
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define all(v) (v).begin(),(v).end()
+#define deb(x) cout<<#x<<" = "<<x<< "\n"
+using namespace std;
+//using namespace __gnu_pbds;
+
+typedef long long int ll;
+typedef long double llf;
+typedef vector<ll> vll;
+typedef pair<ll,ll> pll;
+typedef vector<pll> vpll;
+/*
+template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class T> using multi_ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class T> using r_ordered_set = tree<T, null_type, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class T> using r_multi_ordered_set = tree<T, null_type, greater_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+
+// ordered_set<ll> s;
+// s.order_of_key(k); --> number of items strictly smaller than k
+// s.find_by_order(k); --> k-th item in set (0-indexing) (returns iterator)
+void debvec(vll v){cout<<"deb:: ";for(auto x:v) cout<<x<<" ";cout<<endl;}
+//variables
+
+ll i,j,k;
+
+void S()
+{
+    ll n,m,sum,t,r,a,b,c,x,y,z,lo,hi,q,ans,sol,res,mid,f;
+
+
+    return;
+}
+int main()
+{
+    return 0;
+
+}

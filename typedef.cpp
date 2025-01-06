c0a84c2c09e77efa7489cf5818340122d0316960
Mohammad Abdullah-Al-Sajid Chowdhury <75419355+SajidAbdullah729@users.noreply.github.com>
2025-01-07 05:35:47 +0600

    Create typedef.cpp

---
+++ typedef.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef double lf;
+typedef long double llf;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<vector<ll> > v2d;
+typedef vector<vector<vector<ll> > > v3d;
+typedef pair<ll,ll> pll;
+typedef vector<pll> vpll;
+typedef map<ll,ll> mpl;
+typedef priority_queue<ll> heap;// heap max value from top
+typedef priority_queue<ll, vector<ll>, greater<ll> > revheap;//heap min value from top
+int main()
+{
+   return 0;
+}
